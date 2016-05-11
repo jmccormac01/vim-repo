03e8ac72c6acd9728524d6d5f0f6fe0c6570628d
James McCormac <jmccormac001@gmail.com>
2016-05-11 21:39:58 +0100

    adding installation support for MatchTag

--- .vimrc
+++ .vimrc
@@ -13,3 +13,5 @@ set shiftwidth=4
 set softtabstop=4
 set autoindent
 set smartindent
+
+autocmd FileType html set omnifunc=htmlcomplete#CompleteTags 
--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/zsh
-mkdir bundle && cd bundle
+mkdir bundle && mkdir ftplugin && cd bundle
 git clone git://github.com/tpope/vim-sensible.git
 git clone git://github.com/altercation/vim-colors-solarized.git
 git clone git://github.com/tpope/vim-unimpaired.git
-
+git clone https://github.com/gregsexton/MatchTag.git
+cp -rf MatchTag/ftplugin/* ../ftplugin/
