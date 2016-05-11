40d0aeb0cffdb37f14c3d2e0d2c643d078dda6aa
James McCormac <jmccormac001@gmail.com>
2016-05-11 13:51:22 +0100

    adding an install script to set up vim correctly

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/bin/zsh
+cd bundle
+git clone git://github.com/tpope/vim-sensible.git
+git clone git://github.com/altercation/vim-colors-solarized.git
+git clone git://github.com/tpope/vim-unimpaired.git
+
