1671da5db8f6525f972612959a7d014eafd7ec0b
James McCormac <jmccormac001@gmail.com>
2016-05-11 13:55:56 +0100

    as bundle is not tracked we need to make it first

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/zsh
-cd bundle
+mkdir bundle && cd bundle
 git clone git://github.com/tpope/vim-sensible.git
 git clone git://github.com/altercation/vim-colors-solarized.git
 git clone git://github.com/tpope/vim-unimpaired.git
