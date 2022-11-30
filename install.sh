42ea51df706aae799fa9ce7a557a52378bd100b6
James McCormac <jmccormac001@gmail.com>
2022-11-30 14:38:34 +0000

    adds updated urls to git repos

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/zsh
 mkdir bundle && mkdir ftplugin && cd bundle
-git clone git://github.com/tpope/vim-sensible.git
-git clone git://github.com/altercation/vim-colors-solarized.git
-git clone git://github.com/tpope/vim-unimpaired.git
+git clone git@github.com:tpope/vim-sensible.git
+git clone git@github.com:altercation/vim-colors-solarized.git
+git clone git@github.com:tpope/vim-unimpaired.git
 git clone https://github.com/gregsexton/MatchTag.git
 git clone https://github.com/tmhedberg/SimpylFold.git
 cp -rf MatchTag/ftplugin/* ../ftplugin/
